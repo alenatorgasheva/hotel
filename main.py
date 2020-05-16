103a3593792742bd2645991a7edd2c5cc367f89f
alenatorgasheva <in-fa@mail.ru>
2020-05-16 12:32:17 +0700

    + down sorting

--- main.py
+++ main.py
@@ -9,6 +9,14 @@ import client
 import room
 
 
+def max_price(room_prices):
+    for i in range(len(room_prices)):
+        for j in range(i, len(room_prices) - 1):
+            if room_prices[j][0] < room_prices[j + 1][0]:
+                room_prices[j], room_prices[j + 1] = room_prices[j + 1], room_prices[j]
+    return room_prices
+
+
 def main():
     pass
 
